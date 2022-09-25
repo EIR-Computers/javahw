8679e91f3e6a87d06cb8b809911bc233cd984810
E.I.Radostev <e.i.radostev@gmail.com>
2022-09-25 14:27:39 +0300

    UPDATE

---
+++ PR2/src/TestAuthor.java
@@ -0,0 +1,6 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author Albert = new Author("Albert", "Albert@gmail.com", "Male");
+        System.out.println(Albert.toString());
+    }
+}
